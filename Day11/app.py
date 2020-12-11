9d218cad2d7e1d9db46ed047ba05503e5879b65b
Mitch Eagles <mitch.eagles.stl@gmail.com>
2020-12-11 08:21:03 -0600

    Update app.py

--- Day11/app.py
+++ Day11/app.py
@@ -1,4 +1,4 @@
-
+# use this instead of check_for_visible_neighbors when doing part 1
 def check_for_neighbors(pos_x, pos_y, data):
 	neighbors = 0
 	neighbor_positions = [(pos_x-1, pos_y-1),(pos_x, pos_y-1),(pos_x+1, pos_y-1),
@@ -11,6 +11,31 @@ def check_for_neighbors(pos_x, pos_y, data):
 		elif data[this_neighbor[1]][this_neighbor[0]] == "#":
 			neighbors = neighbors + 1
 	return neighbors
+
+def check_for_visible_neighbors(pos_x, pos_y, data):
+	visible_neighbors = 0
+	directions = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
+	for direction in directions:
+		temp_x = pos_x
+		temp_y = pos_y
+		looking = True
+		while looking:
+			temp_x = temp_x + direction[0]
+			temp_y = temp_y + direction[1]
+			# input((temp_x, temp_y))
+			if temp_x < 0 or temp_y < 0 or temp_x > len(data[0])-1 or temp_y > len(data)-1:
+				# print("Out of bounds")
+				looking = False
+			else:
+				looking_at = data[temp_y][temp_x]
+				if looking_at == 'L':
+					looking = False
+				elif looking_at == '#':
+					visible_neighbors = visible_neighbors + 1
+					looking = False
+	return visible_neighbors
+
+
 
 with open('./Day11/input.txt', 'r') as file:
 	data = file.read()
@@ -32,13 +57,13 @@ with open('./Day11/input.txt', 'r') as file:
 					nextIterationData[pos_y] += '.'
 					continue # floor
 				elif seat_current_value == 'L':
-					if check_for_neighbors(pos_x, pos_y, splitData) == 0:
+					if check_for_visible_neighbors(pos_x, pos_y, splitData) == 0:
 						# change to occupied
 						nextIterationData[pos_y] += '#'
 					else:
 						nextIterationData[pos_y] += 'L'
 				elif seat_current_value == '#':
-					if check_for_neighbors(pos_x, pos_y, splitData) >= 4:
+					if check_for_visible_neighbors(pos_x, pos_y, splitData) >= 5: # If running for part 1, should be 4 instead
 						# change to empty
 						nextIterationData[pos_y] += 'L'
 					else:
